87e259114fe4602c202426719f08f4fd13197d0c
Igor Loskutov <igor.loskutoff@gmail.com>
2025-04-10 22:38:51 -0400

    ai tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 // n bools
-type GameState = boolean[]
+export type GameState = boolean[]
 // unique indices of GameState, and len(Move) < len(GameState)
-type Move = number[]
+export type Move = number[]
 // len(PossibleMoves) <= len(GameState)
-type PossibleMoves = Move[]
+export type PossibleMoves = Move[]
 
 // indices of Moves in PossibleMoves, can repeat, length unbounded
-type Solution = number[]
+export type Solution = number[]
 // one Solution is "all booleans in GameState are true"
 // another Solution is "all booleans in GameState are false"
 // order of Solutions doesn't matter
-type Solutions = [Solution, Solution]
+export type Solutions = [Solution, Solution]
 
 // Apply a move to a game state
-const applyMove = (state: GameState, move: Move): GameState => {
+export const applyMove = (state: GameState, move: Move): GameState => {
   const newState = [...state];
   for (const index of move) {
     newState[index] = !newState[index];
@@ -22,22 +22,22 @@ const applyMove = (state: GameState, move: Move): GameState => {
 };
 
 // Check if all values in the state are the same
-const isAllSame = (state: GameState): boolean => {
+export const isAllSame = (state: GameState): boolean => {
   return state.every(value => value === state[0]);
 };
 
 // Check if all values in the state are true
-const isAllTrue = (state: GameState): boolean => {
+export const isAllTrue = (state: GameState): boolean => {
   return state.every(value => value === true);
 };
 
 // Check if all values in the state are false
-const isAllFalse = (state: GameState): boolean => {
+export const isAllFalse = (state: GameState): boolean => {
   return state.every(value => value === false);
 };
 
 // a Move toggles the booleans of GameState according to the indices
-const solution = (state: GameState, possibleMoves: PossibleMoves): Solutions => {
+export const solution = (state: GameState, possibleMoves: PossibleMoves): Solutions => {
   // First solution: find a path to make all booleans the same (either all true or all false)
   const firstSolution = findSolution(state, possibleMoves);
   
@@ -58,7 +58,7 @@ const solution = (state: GameState, possibleMoves: PossibleMoves): Solutions => 
 };
 
 // BFS to find a solution that makes all booleans the same
-const findSolution = (initialState: GameState, possibleMoves: PossibleMoves): Solution => {
+export const findSolution = (initialState: GameState, possibleMoves: PossibleMoves): Solution => {
   const queue: { state: GameState; path: number[] }[] = [{ state: initialState, path: [] }];
   const visited = new Set<string>();
   
@@ -86,7 +86,7 @@ const findSolution = (initialState: GameState, possibleMoves: PossibleMoves): So
 };
 
 // BFS to find a solution that flips all booleans to the target value
-const findSecondSolution = (initialState: GameState, possibleMoves: PossibleMoves, targetValue: boolean): Solution => {
+export const findSecondSolution = (initialState: GameState, possibleMoves: PossibleMoves, targetValue: boolean): Solution => {
   const queue: { state: GameState; path: number[] }[] = [{ state: initialState, path: [] }];
   const visited = new Set<string>();
   
@@ -112,17 +112,3 @@ const findSecondSolution = (initialState: GameState, possibleMoves: PossibleMove
   
   return []; // No solution found
 };
-
-const gameState = [true, true, true, true, false, false];
-// note that in this specific instance of game, every first index coincides with the index of the possible move itself
-// and also n of possible moves = length of game state
-const possibleMoves = [
-  [0, 1, 3],
-  [1, 0],
-  [2, 5],
-  [3, 0, 4],
-  [4, 5, 3],
-  [5, 4, 2]
-];
-
-console.log(solution(gameState, possibleMoves));
